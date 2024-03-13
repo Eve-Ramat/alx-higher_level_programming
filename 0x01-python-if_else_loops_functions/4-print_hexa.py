e2fba525a44c843ea48ca4bd7dafd82084fb4ffe
Ramatu Aliyu Mustapha <ramatualiyu123@gmail.com>
2024-03-12 17:15:49 -0700

    4³

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for number in range(99):
-    print("{} = {}".format(number, hex(number)))
+for number in range(100):
+    print("{:02d}".format(number), end="")
+    if number != 99:
+        print(", ", end="")
+print()
