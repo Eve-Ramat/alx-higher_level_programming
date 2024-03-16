7a54d2a3a48e9b73971cc75fec4bd6da58899621
Ramatu Aliyu Mustapha <ramatualiyu123@gmail.com>
2024-03-16 11:55:53 -0700

    10-check_cycle.c change

---
+++ 0x00-python-hello_world/indice.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+for index, char in enumerate(str):
+    print(f"Character '{char}' at index {index}")
